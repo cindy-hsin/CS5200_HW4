10b78726ffadc860bbd9f6191f6b755c14aa8073
CHUANHSIN CHEN <cindychen@CHUANHSINdeMacBook-Pro.local>
2022-03-24 23:07:14 -0700

    Finished ReservationsDao

--- src/main/java/restaurant/dal/ReservationsDao.java
+++ src/main/java/restaurant/dal/ReservationsDao.java
@@ -1,5 +1,259 @@
 package restaurant.dal;
 
-public class ReservationsDao {
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
+import restaurant.model.*;
+
+public class ReservationsDao {
+	protected ConnectionManager connectionManager;
+
+	private static ReviewsDao instance = null;
+	protected ReservationsDao() {
+		connectionManager = new ConnectionManager();
+	}
+	public static ReviewsDao getInstance() {
+		if(instance == null) {
+			instance = new ReviewsDao();
+		}
+		return instance;
+	}
+	
+
+	public Reservations create(Reservations reservation) throws SQLException {
+		String insertReservation  =
+				"INSERT INTO Reviews(Start,End,Size,UserName,RestaurantId) " +
+				"VALUES(?,?,?,?,?);";
+		Connection connection = null;
+		PreparedStatement insertStmt = null;
+		ResultSet resultKey = null;
+		
+		
+		try {
+			connection = connectionManager.getConnection();
+			insertStmt = connection.prepareStatement(insertReservation,
+				Statement.RETURN_GENERATED_KEYS);
+
+			insertStmt.setTimestamp(1, new Timestamp(reservation.getStart().getTime()));
+			insertStmt.setTimestamp(2, new Timestamp(reservation.getEnd().getTime()));
+			insertStmt.setInt(3, reservation.getSize());
+			insertStmt.setString(3, reservation.getUser().getUserName());
+			insertStmt.setInt(4, reservation.getRestaurant().getRestaurantId());
+
+			
+			insertStmt.executeUpdate();
+			
+			// Retrieve the auto-generated key and set it, so it can be used by the caller.
+			resultKey = insertStmt.getGeneratedKeys();
+			int reservationId = -1;
+			if(resultKey.next()) {
+				reservationId = resultKey.getInt(1);
+			} else {
+				throw new SQLException("Unable to retrieve auto-generated key.");
+			}
+			reservation.setReservationId(reservationId);
+			
+			// Before returning review, review.created is not set.
+			// Is it a problem???
+			return reservation;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if(connection != null) {
+				connection.close();
+			}
+			if(insertStmt != null) {
+				insertStmt.close();
+			}
+			if(resultKey != null) {
+				resultKey.close();
+			}
+		}
+	}
+	
+	
+	public Reservations getReservationById(int reservationId) throws SQLException {
+		String selectReservation =
+				"SELECT ReservationId,Start,End,Size,UserName,RestaurantId " +
+				"FROM Reservations " +
+				"WHERE ReservationId=?;";
+		Connection connection = null;
+		PreparedStatement selectStmt = null;
+		ResultSet results = null;
+		
+		try {
+			connection = connectionManager.getConnection();
+			selectStmt = connection.prepareStatement(selectReservation);
+			selectStmt.setInt(1, reservationId);
+			results = selectStmt.executeQuery();
+			
+			UsersDao usersDao = UsersDao.getInstance();
+			RestaurantsDao restaurantsDao = RestaurantsDao.getInstance();
+			if(results.next()) {
+				int resultReservationId = results.getInt("ReservationId");
+				Date start =  new Date(results.getTimestamp("Start").getTime());
+				Date end =  new Date(results.getTimestamp("End").getTime());
+				int size = results.getInt("Size");
+				String userName = results.getString("UserName");
+				int restaurantId = results.getInt("RestaurantId");
+				
+				Users user = usersDao.getUserByUserName(userName);
+				Restaurants restaurant = restaurantsDao.getRestaurantById(restaurantId);
+				Reservations reservation = new Reservations(resultReservationId,start,end,size, user, restaurant);
+				return reservation;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if(connection != null) {
+				connection.close();
+			}
+			if(selectStmt != null) {
+				selectStmt.close();
+			}
+			if(results != null) {
+				results.close();
+			}
+		}
+		return null;
+		
+		
+	}
+	public List<Reservations> getReservationsByUserName(String userName) throws SQLException {
+		String selectReservation =
+			"SELECT ReservationId,Start,End,Size,UserName,RestaurantId " +
+					"FROM Reservations " +
+					"WHERE UserName=?;";
+		Connection connection = null;
+		PreparedStatement selectStmt = null;
+		ResultSet results = null;
+		List<Reservations> reservations = new ArrayList<Reservations>();
+		
+		try {
+			connection = connectionManager.getConnection();
+			selectStmt = connection.prepareStatement(selectReservation);
+			selectStmt.setString(1, userName);
+			results = selectStmt.executeQuery();
+			
+			UsersDao usersDao = UsersDao.getInstance();
+			RestaurantsDao restaurantsDao = RestaurantsDao.getInstance();
+			
+			while(results.next()) {
+				int reservationId = results.getInt("ReservationId");
+				Date start =  new Date(results.getTimestamp("Start").getTime());
+				Date end =  new Date(results.getTimestamp("End").getTime());
+				int size = results.getInt("Size");
+				String resultUserName = results.getString("UserName");
+				int restaurantId = results.getInt("RestaurantId");
+
+				
+				Users user = usersDao.getUserByUserName(resultUserName);
+				Restaurants restaurant = restaurantsDao.getRestaurantById(restaurantId);
+				Reservations reservation = new Reservations(reservationId,start,end,size, user, restaurant);
+				reservations.add(reservation);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if(connection != null) {
+				connection.close();
+			}
+			if(selectStmt != null) {
+				selectStmt.close();
+			}
+			if(results != null) {
+				results.close();
+			}
+		}
+		return reservations;
+		
+	}
+	
+	public List<Reservations> getReservationsBySitDownRestaurantId(int restaurantId) throws SQLException {
+		String selectReservation =
+				"SELECT ReservationId,Start,End,Size,UserName,RestaurantId " +
+						"FROM Reservations " +
+						"WHERE RestaurantId=?;";
+			Connection connection = null;
+			PreparedStatement selectStmt = null;
+			ResultSet results = null;
+			List<Reservations> reservations = new ArrayList<Reservations>();
+			
+			try {
+				connection = connectionManager.getConnection();
+				selectStmt = connection.prepareStatement(selectReservation);
+				selectStmt.setInt(1, restaurantId);
+				results = selectStmt.executeQuery();
+				
+				UsersDao usersDao = UsersDao.getInstance();
+				RestaurantsDao restaurantsDao = RestaurantsDao.getInstance();
+				
+				while(results.next()) {
+					int reservationId = results.getInt("ReservationId");
+					Date start =  new Date(results.getTimestamp("Start").getTime());
+					Date end =  new Date(results.getTimestamp("End").getTime());
+					int size = results.getInt("Size");
+					String userName = results.getString("UserName");
+					int resultRestaurantId = results.getInt("RestaurantId");
+
+					Users user = usersDao.getUserByUserName(userName);
+					Restaurants restaurant = restaurantsDao.getRestaurantById(resultRestaurantId);
+					Reservations reservation = new Reservations(reservationId,start,end,size, user, restaurant);
+					reservations.add(reservation);
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+				throw e;
+			} finally {
+				if(connection != null) {
+					connection.close();
+				}
+				if(selectStmt != null) {
+					selectStmt.close();
+				}
+				if(results != null) {
+					results.close();
+				}
+			}
+			return reservations;
+			
+		
+	}
+	
+	public Reservations delete(Reservations reservation) throws SQLException {
+		String deleteReservation = "DELETE FROM Reservations WHERE ReservationId=?;";
+		Connection connection = null;
+		PreparedStatement deleteStmt = null;
+		try {
+			connection = connectionManager.getConnection();
+			deleteStmt = connection.prepareStatement(deleteReservation);
+			deleteStmt.setInt(1, reservation.getReservationId());
+			deleteStmt.executeUpdate();
+
+			// Return null so the caller can no longer operate on the BlogComments instance.
+			return null;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if(connection != null) {
+				connection.close();
+			}
+			if(deleteStmt != null) {
+				deleteStmt.close();
+			}
+		}	
+		
+	}
 }
